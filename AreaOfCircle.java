98d1d5bf947f4f5425603866a3c5f5f69af8cbf3
epsibaranis <epsibaranis@gmail.com>
2023-01-29 16:22:14 +0530

    Area of circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,16 @@
+package AreaOfCircle;
+
+class AreaOfCircle {
+	private float r,a;
+	public void set() {
+        r=150;
+	}
+	public void find() {
+		a=22/7*r*r;
+	}
+	public void display() {
+		System.out.println(a);
+		
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+package AreaOfCircle;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AreaOfCircle m;
+		m=new AreaOfCircle();
+		m.set();
+		m.find();
+		m.display();
+	}
+
+}
